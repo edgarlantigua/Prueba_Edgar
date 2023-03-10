4212526c2ac124e4468bc06f1c5bba499cc7dbae
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2023-03-10 16:39:42 -0300

    Se cambia a SoftwareSerial a la version 7.0.0. Se peude compilar.

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,48 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-#define TX 8
-#define RX 7
-#define config 
-EspSoftwareSerial::UART myport;
+#ifndef D5
+#if defined(ESP8266)
+#define D8 (15)
+#define D5 (14)
+#define D7 (13)
+#define D6 (12)
+#define RX (3)
+#define TX (1)
+#elif defined(ESP32)
+#define D8 (5)
+#define D5 (18)
+#define D7 (23)
+#define D6 (19)
+// Los de Edgar
+#define RX (7)
+#define TX (8)
+#endif
+#endif
+
+#ifdef ESP32
+#define BAUD_RATE 9600
+#else
+#define BAUD_RATE 9600
+#endif
+
+SoftwareSerial testSerial;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Serial Conection Begining");
-  myport.begin(9600, SERIAL_8N1, RX, TX, false);
-  if (!myport)
-  { // If the object did not initialize, then its configuration is invalid
-    Serial.println("Invalid EspSoftwareSerial pin configuration, check config");
-    while (1)
-    { // Don't continue with invalid configuration
-      delay(1000);
-      myport.enableRx(false);
-    }
-  }
+  Serial.begin(115200);
+  testSerial.begin(BAUD_RATE, SWSERIAL_8N1, RX, TX, false);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  Serial.println("Waiting for data");
-  Recopilacion(&myport);
-}
-
-void Recopilacion(EspSoftwareSerial::UART *mp)
-{
-  float dato;
-  mp->enableRx(true);
-  // wait 1 second for the reply from EspSoftwareSerial if any
-  delay(1000);
-  if (mp->listen())
-  {
-    Serial.print("\nResult:");
-    while (mp->listen())
-    {
-      dato = mp->read();
-      Serial.println(dato);
-    }
+  Serial.println("Data from myport:");
+  // while there is data coming in, read it
+  // and send to the hardware serial port:
+  while (testSerial.available() > 0) {
+    char inByte = testSerial.read();
+    Serial.write(inByte);
   }
+  // blank line to separate data from the two ports:
+  Serial.println();
+}
